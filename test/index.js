1728601ed44443cfc58adad41a090f849196643b
Julien Hamaide <julien.hamaide@fishingcactus.com>
2016-02-28 22:27:52 +0100

    First tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,41 @@
 'use strict';
 
 var assert = require('assert');
+require('mocha-sinon');
+var mockfs = require('mock-fs');
 var cascadingFileProperties = require('../lib');
 
 describe('cascading-file-properties', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('should callback when done', function () {
+    var callback = this.sinon.spy();
+
+    var context = new cascadingFileProperties.Context();
+
+    context.loadProperties('a{}', callback);
+    assert(callback.called);
+    assert(callback.calledWith());
+  });
+
+  it('should fail on wrong data', function () {
+    var callback = this.sinon.spy();
+
+    var context = new cascadingFileProperties.Context();
+
+    context.loadProperties('a{ %%% }', callback);
+    assert(callback.called);
+  });
+
+  it('should load file and call loadProperties in loadPropertyFile', function () {
+    var context = new cascadingFileProperties.Context();
+    var spy = this.sinon.spy(context, 'loadProperties');
+    var callback = this.sinon.spy();
+
+    mockfs({
+      'data/test.cfp': 'test {}'
+    });
+
+    context.loadPropertyFile('data/test.cfp', callback);
+    spy.calledWith('test {}', callback, 'data/test.cfp');
+    callback.calledWith();
   });
 });
